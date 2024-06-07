ca9676fec5a8757cee4fe20e8e1fe62cdf025894
xliu <xun.liu.1991@gmail.com>
2024-06-07 06:19:00 -0500

    test string function

--- example.c
+++ example.c
@@ -18,28 +18,32 @@
 #pragma warning(disable: 4127)
 #endif
 
+const char *getscheme() {
+  return "GETFILE";
+}
+
 /* Tests are functions that return void, and take a single void*
  * parameter.  We'll get to what that parameter is later. */
 static MunitResult
 test_compare(const MunitParameter params[], void* data) {
   /* We'll use these later */
-  const unsigned char val_uchar = 'b';
-  const short val_short = 1729;
-  double pi = 3.141592654;
-  char* stewardesses = "stewardesses";
-  char* most_fun_word_to_type;
+  // const unsigned char val_uchar = 'b';
+  // const short val_short = 1729;
+  // double pi = 3.141592654;
+  char* stewardesses = "GETFILE";
+  // char* most_fun_word_to_type;
 
   /* These are just to silence compiler warnings about the parameters
    * being unused. */
-  (void) params;
-  (void) data;
+  // (void) params;
+  // (void) data;
 
   /* Let's start with the basics. */
-  munit_assert(0 != 1);
+  // munit_assert(0 != 1);
 
   /* There is also the more verbose, though slightly more descriptive
      munit_assert_true/false: */
-  munit_assert_false(0);
+  // munit_assert_false(0);
 
   /* You can also call munit_error and munit_errorf yourself.  We
    * won't do it is used to indicate a failure, but here is what it
@@ -48,19 +52,19 @@ test_compare(const MunitParameter params[], void* data) {
   /* munit_errorf("Goodbye, cruel %s", "world"); */
 
   /* There are macros for comparing lots of types. */
-  munit_assert_char('a', ==, 'a');
+  // munit_assert_char('a', ==, 'a');
 
   /* Sure, you could just assert('a' == 'a'), but if you did that, a
    * failed assertion would just say something like "assertion failed:
    * val_uchar == 'b'".  µnit will tell you the actual values, so a
    * failure here would result in something like "assertion failed:
    * val_uchar == 'b' ('X' == 'b')." */
-  munit_assert_uchar(val_uchar, ==, 'b');
+  // munit_assert_uchar(val_uchar, ==, 'b');
 
   /* Obviously we can handle values larger than 'char' and 'uchar'.
    * There are versions for char, short, int, long, long long,
    * int8/16/32/64_t, as well as the unsigned versions of them all. */
-  munit_assert_short(42, <, val_short);
+  // munit_assert_short(42, <, val_short);
 
   /* There is also support for size_t.
    *
@@ -68,10 +72,10 @@ test_compare(const MunitParameter params[], void* data) {
    * "uncopyrightables", which has uncopyrightable (and
    * dermatoglyphics, which is the study of fingerprints) beat by a
    * character */
-  munit_assert_size(strlen("uncopyrightables"), >, strlen("dermatoglyphics"));
+  // munit_assert_size(strlen("uncopyrightables"), >, strlen("dermatoglyphics"));
 
   /* Of course there is also support for doubles and floats. */
-  munit_assert_double(pi, ==, 3.141592654);
+  // munit_assert_double(pi, ==, 3.141592654);
 
   /* If you want to compare two doubles for equality, you might want
    * to consider using munit_assert_double_equal.  It compares two
@@ -79,7 +83,7 @@ test_compare(const MunitParameter params[], void* data) {
    * Note that precision (the third argument to the macro) needs to be
    * fully evaluated to an integer by the preprocessor so µnit doesn't
    * have to depend pow, which is often in libm not libc. */
-  munit_assert_double_equal(3.141592654, 3.141592653589793, 9);
+  // munit_assert_double_equal(3.141592654, 3.141592653589793, 9);
 
   /* And if you want to check strings for equality (or inequality),
    * there is munit_assert_string_equal/not_equal.
@@ -88,24 +92,24 @@ test_compare(const MunitParameter params[], void* data) {
    * keyboard with only one hand, which makes it loads of fun to type.
    * If I'm going to have to type a string repeatedly, let's make it a
    * good one! */
-  munit_assert_string_equal(stewardesses, "stewardesses");
+  munit_assert_string_equal(stewardesses, getscheme());
 
   /* A personal favorite macro which is fantastic if you're working
    * with binary data, is the one which naïvely checks two blobs of
    * memory for equality.  If this fails it will tell you the offset
    * of the first differing byte. */
-  munit_assert_memory_equal(7, stewardesses, "steward");
+  // munit_assert_memory_equal(7, stewardesses, "steward");
 
   /* You can also make sure that two blobs differ *somewhere*: */
-  munit_assert_memory_not_equal(8, stewardesses, "steward");
+  // munit_assert_memory_not_equal(8, stewardesses, "steward");
 
   /* There are equal/not_equal macros for pointers, too: */
-  most_fun_word_to_type = stewardesses;
-  munit_assert_ptr_equal(most_fun_word_to_type, stewardesses);
+  // most_fun_word_to_type = stewardesses;
+  // munit_assert_ptr_equal(most_fun_word_to_type, stewardesses);
 
   /* And null/not_null */
-  munit_assert_null(NULL);
-  munit_assert_not_null(most_fun_word_to_type);
+  // munit_assert_null(NULL);
+  // munit_assert_not_null(most_fun_word_to_type);
 
   /* Lets verify that the data parameter is what we expected.  We'll
    * see where this comes from in a bit.
@@ -113,7 +117,7 @@ test_compare(const MunitParameter params[], void* data) {
    * Note that the casting isn't usually required; if you give this
    * function a real pointer (instead of a number like 0xdeadbeef) it
    * would work as expected. */
-  munit_assert_ptr_equal(data, (void*)(uintptr_t)0xdeadbeef);
+  // munit_assert_ptr_equal(data, (void*)(uintptr_t)0xdeadbeef);
 
   return MUNIT_OK;
 }
@@ -280,11 +284,11 @@ static MunitTest test_suite_tests[] = {
      *
      * For our example we don't really need a fixture, but lets
      * provide one anyways. */
-    test_compare_setup,
+    // test_compare_setup,
     /* If you passed a callback for the fixture setup function, you
      * may want to pass a corresponding callback here to reverse the
      * operation. */
-    test_compare_tear_down,
+    // test_compare_tear_down,
     /* Finally, there is a bitmask for options you can pass here.  You
      * can provide either MUNIT_TEST_OPTION_NONE or 0 here to use the
      * defaults. */
